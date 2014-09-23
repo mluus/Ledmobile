767cb6d5d9343a1976db238045a300d808c8a540
MLuus <morne.luus@gmail.com>
2014-09-23 13:13:40 +0800

    before cordova upgrade

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,34 @@
-var yourname = {};
+var yourname = {}
+var currentTokens
+var getTokens = function() {
+         console.log('autoload')
+        var engineerUuid = device.uuid;
+        console.log(engineerUuid);
+        var actualIp = "192.168.2.2:3000"
+        var URL = "http://" + actualIp + "/engineer/" + engineerUuid
+        console.log(URL)
+        object = {
+            url:  URL,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            processData: false,
+            // data: JSON.stringify(myData),
+            success: function(resp){
+                // currentTokens = resp
+                console.log(currentTokens)
+                sentence = 'Available Tokens:' +  " " + resp
+                var element = document.getElementById("header");
+                element.innerHTML = sentence;
+            }
+        }
+        $.ajax(object);
+        return false;
+
+    }
+
+
+
 yourname.app = {
 
     initialize: function() {
@@ -8,17 +38,41 @@ yourname.app = {
     onDeviceReady: function() {
   		FastClick.attach(document.body);
       yourname.app.receivedEvent('deviceready');
+      setInterval(function(){getTokens()}, 1000);
       },
 
     receivedEvent: function(id) {
+        var switchOn = function() {
+            $(".ui-mobile .ui-page").css("background-color","#4dad49");
+            console.log('notice the server that the phone is in the beacon region')
+            var engineerUuid = device.uuid;
+            console.log(engineerUuid);
+            var actualIp = "192.168.2.2:3000"
+            var URL = "http://" + actualIp + "/engineer/" + engineerUuid
+            console.log(URL)
+            object = {
+                url:  URL,
+                type: 'PUT',
+                dataType: 'json',
+                contentType: 'application/json',
+                processData: false,
+                // data: JSON.stringify(myData),
+                success: function(resp){
+                }
+            }
+            $.ajax(object);
+            return false;
+        }
 
         console.log('here the functions works');
+
+
 
     /**
  * Function that creates a BeaconRegion data transfer object.
  *
  * @throws Error if the BeaconRegion parameters are not valid.
- */
+   */
         function createBeacon() {
 
             var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9'; // mandatory
@@ -45,50 +99,112 @@ yourname.app = {
           document.body.appendChild(br2);
 
           window.scrollTo(0, window.document.height);
-      };
+        };
 
-      var doSomething = function (state) {
-            if (state === "CLRegionStateOutside") {
-                return console.log('out');
-            } else if (state === "CLRegionStateInside") {
-                return console.log('in')
-            } else {
-                return console.log('else');
-            }
-        };
+        var doSomething = function (state) {
+            if (state === "CLRegionStateOutside") {
+                return $(".ui-mobile .ui-page").css("background-color","white");
+            } else if (state === "CLRegionStateInside") {
+                return switchOn();
+            } else {
+                return console.log('else');
+            }
+        };
 
-      var delegate = new cordova.plugins.locationManager.Delegate().implement({
+        var delegate = new cordova.plugins.locationManager.Delegate().implement({
 
-          didDetermineStateForRegion: function (pluginResult) {
+            didDetermineStateForRegion: function (pluginResult) {
 
-              logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+                logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
 
-              cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
-                  + JSON.stringify(pluginResult));
-              doSomething(pluginResult.state)
-          },
+                cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+                    + JSON.stringify(pluginResult));
+                doSomething(pluginResult.state)
+            },
 
-          didStartMonitoringForRegion: function (pluginResult) {
-              console.log('didStartMonitoringForRegion:', pluginResult);
+            didStartMonitoringForRegion: function (pluginResult) {
+                console.log('didStartMonitoringForRegion:', pluginResult);
 
-              logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
-          },
+                logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+            },
 
-          didRangeBeaconsInRegion: function (pluginResult) {
-              logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
-          }
+            didRangeBeaconsInRegion: function (pluginResult) {
+                logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+            }
 
-      });
+        });
 
-      var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
-      var identifier = 'beaconOnTheMacBooksShelf';
-      var minor = 1000;
-      var major = 5;
-      var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+        var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
+        var identifier = 'beaconOnTheMacBooksShelf';
+        var minor = 1000;
+        var major = 5;
+        var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
 
-      cordova.plugins.locationManager.setDelegate(delegate);
-      cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
-          .fail(console.error)
-          .done();
+        cordova.plugins.locationManager.setDelegate(delegate);
+        cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+            .fail(console.error)
+            .done();
+
+        $('#click').click(function(){
+            console.log('clicked')
+            var password = $("#password").val();
+            var engineerUuid = device.uuid;
+            console.log(engineerUuid);
+            console.log(password)
+            var actualIp = "192.168.2.2:3000"
+            var URL = "http://" + actualIp + "/engineer/associate/" + engineerUuid + "/to/" + password
+            console.log(URL)
+             // myData =  {engineer:
+             //            {
+             //             device: cardholderUuid,
+             //             password: password,}}
+            object = {
+                url:  URL,
+                type: 'PUT',
+                dataType: 'json',
+                contentType: 'application/json',
+                processData: false,
+                // data: JSON.stringify(myData),
+                success: function(resp){
+                }
+            }
+            $.ajax(object);
+            $(':mobile-pagecontainer').pagecontainer('change', '#next', {
+                transition: 'flip',
+                changeHash: false,
+                reverse: true,
+                showLoadMsg: true
+            });
+            return false;
+        });
+
+        $('#click2').click(function(){
+            console.log('clicked2')
+            var engineerUuid = device.uuid;
+            console.log(engineerUuid);
+            var actualIp = "192.168.2.2:3000"
+            var URL = "http://" + actualIp + "/engineer/" + engineerUuid
+            console.log(URL)
+             // myData =  {engineer:
+             //            {
+             //             device: cardholderUuid,
+             //             password: password,}}
+            object = {
+                url:  URL,
+                type: 'GET',
+                dataType: 'json',
+                contentType: 'application/json',
+                processData: false,
+                // data: JSON.stringify(myData),
+                success: function(resp){
+                    currentTokens = resp
+                    console.log(currentTokens)
+                    $( "p" ).append(currentTokens);
+                }
+            }
+            $.ajax(object);
+            return false;
+        });
+
     }
 };
